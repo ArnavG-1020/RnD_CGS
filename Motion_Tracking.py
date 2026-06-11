3394bc5d4cd90d3f2e236bde9f8925fea79d2de0
ArnavG-1020 <gupta.arnav1020@gmail.com>
2026-06-11 18:29:02 +0530

    Elbow Angles Calculation

--- Motion_Tracking.py
+++ Motion_Tracking.py
@@ -3,8 +3,35 @@ import numpy as np
 import mediapipe as mp
 import socket
 import json
+import math
 
-#def angle_calc()
+
+def to_blender_space(landmark):
+    """Converts MediaPipe screen coordinates to Blender 3D coordinates."""
+    bx = landmark.x
+    by = landmark.z
+    bz = -landmark.y
+    return bx,by,bz
+
+def calculate_3d_angles(parent_joint, child_joint):
+    """Calculates Pitch (Up/Down) and Yaw (Left/Right) in radians."""
+    p_x, p_y, p_z = to_blender_space(parent_joint)
+    c_x, c_y, c_z = to_blender_space(child_joint)
+    dx = c_x - p_x
+    dy = c_y - p_y
+    dz = c_z - p_z
+    #Yaw: Left/Right swing on the X/Y (Depth) plane
+    yaw = math.atan2(dx, dy)
+    # Pitch: Up/Down swing using the Z axis and horizontal distance
+    horizontal_distance = math.sqrt(dx**2 + dy**2)
+    pitch = math.atan2(dz, horizontal_distance)
+    return pitch, yaw
+
+
+def len_calc(x,y,z):
+     length = math.sqrt(x**2 + y**2 + z**2)
+     return length
+     
 
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 blender_address = ("127.0.0.1", 5052)
@@ -26,12 +53,27 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         mp_drawing.draw_landmarks(img, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
         mp_drawing.draw_landmarks(img, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS)
 
-        if results.right_hand_landmarks:
-            index_tip = results.right_hand_landmarks.landmark[mp_holistic.HandLandmark.INDEX_FINGER_TIP]
-            tip_x = index_tip.x
-            tip_y = index_tip.y
-            tip_z= index_tip.z
-            print(f"Index Finger Tip -> X: {tip_x:.3f} | Y: {tip_y:.3f} | Z: {tip_z:.3f}")
+        if results.pose_landmarks:
+                '''index_tip = results.right_hand_landmarks.landmark[mp_holistic.HandLandmark.INDEX_FINGER_TIP]'''
+                elbow = results.pose_landmarks.landmark[mp_holistic.PoseLandmark.RIGHT_ELBOW]
+                wrist = results.pose_landmarks.landmark[mp_holistic.PoseLandmark.RIGHT_WRIST]
+
+                live_pitch, live_yaw = calculate_3d_angles(elbow, wrist)
+
+                data = {
+                     'RightForeArm' : [live_pitch, 0.0, live_yaw]
+                }
+                '''
+                lm = results.right_hand_landmarks.landmark[elbow]
+                ax = math.degrees(math.acos(lm.x/len_calc(lm.x,lm.y,lm.z)))
+                ay = math.degrees(math.acos(lm.y/len_calc(lm.x,lm.y,lm.z)))
+                az = math.degrees(math.acos(lm.z/len_calc(lm.x,lm.y,lm.z)))
+                print(f"X={ax}, Y={ay}, Z={az}")
+                '''
+
+                message = json.dumps(data).encode('utf-8')
+                print(f"SENDING: {data}")
+                udp_socket.sendto(message, blender_address)
 
         cv2.imshow('WebCam', img)
         if cv2.waitKey(10) & 0xFF == ord('q'):
